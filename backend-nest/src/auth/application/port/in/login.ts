6040169842a6ee45bfca8930dc2b03b6e867314b
jordanhuaman <jordanelcracken@gmail.com>
2026-04-19 17:32:56 -0500

    feat: enhance login functionality with error handling and JWT token generation

--- backend-nest/src/auth/application/port/in/login.ts
+++ backend-nest/src/auth/application/port/in/login.ts
@@ -2,19 +2,21 @@ import { UserRepository } from 'src/auth/domain/user.repository';
 import { LoginUseCase } from '../../usecase/login.usecase';
 import { Injectable } from '@nestjs/common';
 import { PasswordHashI } from '../out/password.provider';
+import { JwtProvider } from '../out/jwt.provider';
 
 @Injectable()
 export class Login implements LoginUseCase {
   constructor(
     private readonly repository: UserRepository,
     private readonly passwordProvider: PasswordHashI,
+    private readonly jwtProvider: JwtProvider,
   ) {}
 
   async execute(email: string, password: string): Promise<string> {
     const result = await this.repository.findByEmail(email);
 
     if (result == null) {
-      throw new Error('');
+      throw new Error('User not found');
     }
 
     const comparePassword = await this.passwordProvider.compare(
@@ -23,9 +25,11 @@ export class Login implements LoginUseCase {
     );
 
     if (!comparePassword) {
-      throw new Error('');
+      throw new Error('Password not the same');
     }
 
-    return '';
+    const token = this.jwtProvider.generateTokens({ email: result.userName });
+
+    return token.accessToken;
   }
 }
--- backend-nest/src/auth/infraestructure/in/rest/auth.controller.ts
+++ backend-nest/src/auth/infraestructure/in/rest/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { Login } from 'src/auth/application/port/in/login';
 import type { UserLoginI } from 'src/shared/@types/user';
 
@@ -6,7 +6,7 @@ import type { UserLoginI } from 'src/shared/@types/user';
 export class AuthController {
   constructor(private readonly loginUsecase: Login) {}
 
-  @Get('/login')
+  @Post('/login')
   async login(@Body() request: UserLoginI): Promise<string> {
     const { email, password } = request;
     return await this.loginUsecase.execute(email, password);
